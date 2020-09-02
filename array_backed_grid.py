1e46bda993e2cda0564adf72f9ad616752701efb
georgiablanco <georgia.litchfield@hotmail.co.uk>
2020-09-02 20:27:51 +0100

    Initial commit

---
+++ array_backed_grid.py
@@ -0,0 +1,41 @@
+"""
+ Example program to show using an array to back a grid on-screen.
+
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+
+ Explanation video: http://youtu.be/mdTeqiWyFnc
+"""
+
+
+class GridCreator:
+    def __init__(self, width, height, margin):
+        self.width = width
+        self.height = height
+        self.margin = margin
+
+    def create_2d_array(self):
+
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        grid = []
+        for row in range(10):
+            # Add an empty array that will hold each cell
+            # in this row
+            grid.append([])
+            for column in range(10):
+                grid[row].append(0)  # Append a cell
+
+        # Set row 1, cell 5 to one. (Remember rows and
+        # column numbers start at zero.)
+        grid[1][5] = 1
+
+        return grid
+
+
+
+if __name__ == "__main__":
+    grid = GridCreator(20, 20, 5).create_2d_array()
+    print(grid)
---
+++ grid_colour_changer.py
@@ -0,0 +1,84 @@
+"""
+Class for the colour changing
+"""
+import pygame
+from array_backed_grid import GridCreator
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+class ColourChangerGame:
+    def __init__(self, width, height, margin, window_size):
+        self.width = width
+        self.height = height
+        self.margin = margin
+        self.window_size = window_size
+
+    def user_input(self):
+        grid = GridCreator(self.width, self.height, self.margin).create_2d_array()
+
+        print(grid)
+
+        # Initialize pygame
+        pygame.init()
+
+        # Set the HEIGHT and WIDTH of the screen
+        screen = pygame.display.set_mode(self.window_size)
+
+        # Set title of screen
+        pygame.display.set_caption("Array Backed Grid")
+
+        # Loop until the user clicks the close button.
+        done = False
+
+        # Used to manage how fast the screen updates
+        clock = pygame.time.Clock()
+
+        # -------- Main Program Loop -----------
+        while not done:
+            for event in pygame.event.get():  # User did something
+                if event.type == pygame.QUIT:  # If user clicked close
+                    done = True  # Flag that we are done so we exit this loop
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # User clicks the mouse. Get the position
+                    pos = pygame.mouse.get_pos()
+                    # Change the x/y screen coordinates to grid coordinates
+                    column = pos[0] // (self.width + self.margin)
+                    row = pos[1] // (self.height + self.margin)
+                    # Set that location to one
+                    grid[row][column] = 1
+                    print("Click ", pos, "Grid coordinates: ", row, column)
+
+            # Set the screen background
+            screen.fill(BLACK)
+
+            # Draw the grid
+            for row in range(10):
+                for column in range(10):
+                    color = WHITE
+                    if grid[row][column] == 1:
+                        color = GREEN
+                    pygame.draw.rect(screen,
+                                     color,
+                                     [(self.margin + self.width) * column + self.margin,
+                                      (self.margin + self.height) * row + self.margin,
+                                      self.width,
+                                      self.height])
+
+            # Limit to 60 frames per second
+            clock.tick(60)
+
+            # Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
+
+        # Be IDLE friendly. If you forget this line, the program will 'hang'
+        # on exit.
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    ColourChangerGame(20, 20, 5, [255, 255])
